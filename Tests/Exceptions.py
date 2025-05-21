0b855051393c30f7536e25777ca046a944156c9b
23f2000237 <142656970+23f2000237@users.noreply.github.com>
2025-05-21 16:20:50 +0530

    A testing Class for Unittests

---
+++ Tests/Exceptions.py
@@ -0,0 +1,8 @@
+class NoFunctionException(Exception):
+    def __init__(self,stmt):
+        self.stmt=stmt
+    def __str__(self):
+        return self.stmt
+class UnequalTestSetException(Exception):
+    def __init__(self):
+        self.stmt="There is a mismatch between the number of Test cases and The number of expected cases"
