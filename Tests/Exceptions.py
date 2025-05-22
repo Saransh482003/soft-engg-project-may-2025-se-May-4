7a94a68ddf955f80a9167fdd5c7ed49a8f96f5a6
23f2000237 <142656970+23f2000237@users.noreply.github.com>
2025-05-22 23:34:07 +0530

    Update Exceptions.py

--- Tests/Exceptions.py
+++ Tests/Exceptions.py
@@ -6,3 +6,5 @@ class NoFunctionException(Exception):
 class UnequalTestSetException(Exception):
     def __init__(self):
         self.stmt="There is a mismatch between the number of Test cases and The number of expected cases"
+    def __str__(self):
+        return self.stmt
